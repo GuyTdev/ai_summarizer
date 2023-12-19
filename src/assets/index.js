7457c7c937a4fa1b9643121196bac7c246af3203
Guy T Dev <guytdev@gmail.com>
2023-12-19 13:52:45 +0200

    fix enter icon in Edge

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import loader from './loader.svg'
 import copy from './copy.svg'
 import logo from './logo.svg'
 import tick from './tick.svg'
-import enter from './key-enter.svg'
+import enter from './enter.png'
 
 export {
     linkIcon,
